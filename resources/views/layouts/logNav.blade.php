44cc34f45519ef36a0b8a707126fae2ae001b3da
H∆DI <hadi@thiqatech.com>
2024-02-22 12:00:17 +0400

    updated text style

--- resources/views/layouts/logNav.blade.php
+++ resources/views/layouts/logNav.blade.php
@@ -6,9 +6,9 @@
         </a>
     </nav>
     <h5 class="text-monospace sm-text-white">
-        <h2>Welcome to OTP SMS for Verification in Nigeria</h2>
+        <h2 class="sm-text-white">Welcome to OTP SMS for Verification in Nigeria</h2>
       <div align="justify" class="sm-text-white"><p>This platform is designed for corporate and business entities who desire to send transactional OTP SMS messages. This SMS gateway bypasses Do Not Disturb (DND) subscription and has 99.9% delivery rate to valid and active numbers. Messages can only be sent here with approved Sender ID. OTP SMS for verification in Nigeria serves as a crucial authentication method in the digital landscape, ensuring secure access to online platforms and services. One-Time Passwords (OTPs) are sent via SMS to users' mobile devices, providing a unique code that validates their identity during login or transaction processes. With the rise of online transactions and the need for heightened security measures, OTP SMS verification has become indispensable for businesses across various sectors in Nigeria. <br><br>Whether it's banking, e-commerce, or social media platforms, OTP SMS adds an extra layer of security, safeguarding users' accounts from unauthorized access and fraudulent activities. In a country where digital adoption is rapidly increasing, OTP SMS verification plays a vital role in building trust and confidence among users, reassuring them of the safety and integrity of their online interactions. By leveraging OTP SMS for verification, businesses not only enhance security but also streamline user authentication processes, fostering a seamless and trustworthy digital experience for their customers across Nigeria.</p>
-        <p>Unapproved sender Ids cannot send message here. Messages sent here must be in agreement with your approved application. Sign up and find out how to register.</p></div>
+        <p class="sm-text-white">Unapproved sender Ids cannot send message here. Messages sent here must be in agreement with your approved application. Sign up and find out how to register.</p></div>
     </h5>
     <img src="{{asset('assets/images/1656167780804.png')}}" alt="Welcome to OTP SMS for Verification in Nigeria" style="width:350px;height:200px">
 </div>
@@ -16,7 +16,7 @@
 <style>
     @media screen and (max-width 768px){
         .sm-text-white{
-            color: white;
+            color: white !important;
         }
     }
 </style>
